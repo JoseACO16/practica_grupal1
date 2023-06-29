164da97ed36692914b1f7f891ac09e258d86425f
JoseCastillo16 <106782113+JoseCastillo16@users.noreply.github.com>
2023-06-28 23:02:22 -0600

    pasos 2 ,3

---
+++ src/test/java/Practica_Grupal/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica_Grupal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
